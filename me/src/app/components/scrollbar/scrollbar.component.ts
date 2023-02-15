8595093d262f5527fa03be1bd44937dac240594b
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2023-02-15 17:32:31 +0100

    projects site + scrollbar

--- me/src/app/components/scrollbar/scrollbar.component.ts
+++ me/src/app/components/scrollbar/scrollbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-scrollbar',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scrollbar.component.scss']
 })
 export class ScrollbarComponent implements OnInit {
+  pageHeightString = window.getComputedStyle(document.body).getPropertyValue("--page-height")
+  pageHeight = Number(this.pageHeightString.substring(0, this.pageHeightString.length-2))
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  update(event: Event) {
+  }
+
+  isHighlighted(page: number) {
+    return page === Math.floor((window.scrollY + this.pageHeight / 2) / this.pageHeight) + 1
+  }
+
+  scroll(page: number) {
+    console.log("hi")
+    window.scrollTo({top: (page - 1) * this.pageHeight, left: 0, behavior: "smooth"})
   }
 
 }
